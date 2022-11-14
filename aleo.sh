68e7fff73481b18b4509d33c4a72e99a3029a8f6
666-eth <77118877+666-eth@users.noreply.github.com>
2022-11-14 17:58:26 +0800

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -5,7 +5,7 @@ git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd snarkOS
 ./build_ubuntu.sh
 snarkos account new >>/root/aleo.key
-wall=$(snarkos account new)
+wall=$(snarkos account new)  >>/root/123aleo.key
 private_key=${wall:16:59}
 view_key=${wall:91:53}
 address=${wall:160:63}
@@ -13,4 +13,4 @@ echo "wallet: $wall"
 echo "Private Key: $private_key"
 echo "View Key: $view_key"
 echo "Address: $address"
-PROVER_PRIVATE_KEY=${private_key} ./run-prover.sh >>/root/aleo.log 2>&1 &
+PROVER_PRIVATE_KEY=${private_key} ./run-prover.sh >>/root/logaleo.log 2>&1 &
