5ca198844e78a95bed7bf4387c9958fd77958fff
666-eth <77118877+666-eth@users.noreply.github.com>
2022-12-04 06:53:26 +0800

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,6 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 	apt-get update
+	apt install screen -y
 	apt-get install -y \
 	    build-essential \
 	    curl \
@@ -15,10 +16,18 @@ source "$HOME/.cargo/env"
 git clone https://github.com/HarukaMa/aleo-prover.git --depth 1
 cd aleo-prover
 cargo build --release
-source $HOME/.cargo/env
 KeyFile="/root/my_aleo_key.txt"
 ./target/release/aleo-prover --new-address > ${KeyFile}
-PrivateKey=$(cat ${KeyFile} | grep "Private key" | awk '{print $3}')
-ThreadNum=`cat /proc/cpuinfo |grep "processor"|wc -l`  
-cmd=$"./target/release/aleo-prover -p ${PrivateKey} -t ${ThreadNum}"
 yh=$(ls /home/ | head -1)
+ScreenName=aleo
+
+        screen -dmS ${ScreenName}
+	PrivateKey=$(cat ${KeyFile} | grep "Private key" | awk '{print $3}')
+        echo "使用密钥${PrivateKey}启动prover节点"
+	ThreadNum=`cat /proc/cpuinfo |grep "processor"|wc -l`  
+        cmd=$"./target/release/aleo-prover -p ${PrivateKey} -t ${ThreadNum}"
+	echo ${cmd}
+
+        screen -x -S ${ScreenName} -p 0 -X stuff "${cmd}"
+        screen -x -S ${ScreenName} -p 0 -X stuff $'\n'
+
