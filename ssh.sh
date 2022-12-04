2298cd0146aab4da7979aacbfa7e9690b97c1b17
666-eth <77118877+666-eth@users.noreply.github.com>
2022-12-05 02:57:41 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo yes
+sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g"   /etc/ssh/sshd_config
+sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g"   /etc/ssh/sshd_config
+systemctl restart ssh
